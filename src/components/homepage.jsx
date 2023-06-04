1ca79a6ded5c056aba294d2677e60aa692004293
Mav023 <gabrielluismaverick@gmail.com>
2023-06-05 02:07:47 +0800

    started main page

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -3,7 +3,7 @@ import React, {useEffect, useState} from "react";
 import "./styles/mainpage.css"
 import "./styles/homepage.css"
 
-
+import ProfileImage from './assets/profile-you.png'
 
 import NavigationBar from "./navigationBar";
 import TopBar from "./topBar";
@@ -28,14 +28,33 @@ const HomePage = () => {
       <div class="mainpage-container">
         {screenWidth >= 768 && <NavigationBar selected={'Home'}/>}
         <div class="homepage-container">
-          //dito na yung sa gitna sa homepage to<br/>
-          //scrollable na yan
+          <div class="stories-container">
+              <Story username="Marebik"/>
+              <Story username="Marebik"/>
+              <Story username="Marebik"/>
+              <Story username="Marebik"/>
+              <Story username="Marebik"/>
+              <Story username="Marebik"/>
+          </div>
         </div>
         {screenWidth >= 768 && <FriendsList/>}
       </div>
     </>
   );
 };
+
+const Story = ({ username }) => {
+  return (
+    <div class="story">
+      <div class="story-image-container">
+        <img class="story-image" src={ProfileImage} alt="story"/>
+      </div>
+      <p class="story-username">
+        {username} 
+      </p>
+    </div>
+  );
+}
 
 
 
